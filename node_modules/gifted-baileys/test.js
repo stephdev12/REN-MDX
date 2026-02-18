6c92a41d3425706dcb43422e65a2093c98e4700f
root <root@srv1308921.hstgr.cloud>
2026-02-18 20:27:43 +0000

    Fix: Remove ignored files (node_modules, session) from git tracking

--- node_modules/gifted-baileys/test.js
+++
@@ -1,7 +0,0 @@
-const baileys = require('./lib/index.js');
-
-console.log('=== gifted-baileys Library Test ===\n');
-console.log('Library loaded successfully!');
-console.log('\nAvailable exports:');
-console.log(Object.keys(baileys).join('\n'));
-console.log('\n=== Library is ready to use ===');
